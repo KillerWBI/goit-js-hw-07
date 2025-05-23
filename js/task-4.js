502943f72225f25c72885c0ba8d1ca15198c94e5
GetUp <108660418+KillerWBI@users.noreply.github.com>
2025-05-23 23:11:11 +0200

    wqe

--- js/task-4.js
+++ js/task-4.js
@@ -1,1 +1,22 @@
+const form = document.querySelector('.login-form');
 
+form.addEventListener('submit', (event) => {
+  event.preventDefault();
+
+  // const { email, password } = form.elements;
+
+  const emailValue = form.elements.email.value.trim();
+  const passwordValue = form.elements.password.value.trim();
+
+
+  if (emailValue !== '' && passwordValue !== '') {
+    const formData = {
+      email: emailValue,
+      password: passwordValue,
+    };
+    console.log(formData);
+    form.reset();
+    return;
+  }
+  alert('All form fields must be filled in');
+});
